23a214e823128dee51f70c56d046e1d67be633bb
Elias Alexander Wiklund Ottersbo <realeawo5@gmail.com>
2026-09-08 22:11:58 +0200

    fix: add os detection for compiler suffix

--- cmake/aarch64-linux-gnu.cmake
+++ cmake/aarch64-linux-gnu.cmake
@@ -8,14 +8,22 @@ if(NOT DEFINED ENV{ARM_GCC_ROOT})
     )
 endif()
 
+# Set the root of the aarch64-none-linux-gnu toolchain from the environment variable
 set(ARM_GCC_ROOT "$ENV{ARM_GCC_ROOT}")
 
+# Determine if the host system is Windows, and set the executable suffix accordingly
+if(WIN32)
+    set(EXE_SUFFIX ".exe")
+else()
+    set(EXE_SUFFIX "")
+endif()
+
+# Set the compilers to the aarch64-none-linux-gnu toolchain
 set(CMAKE_C_COMPILER
-    "${ARM_GCC_ROOT}/bin/aarch64-none-linux-gnu-gcc")
+    "${ARM_GCC_ROOT}/bin/aarch64-none-linux-gnu-gcc${EXE_SUFFIX}")
 
 set(CMAKE_CXX_COMPILER
-    "${ARM_GCC_ROOT}/bin/aarch64-none-linux-gnu-g++")
+    "${ARM_GCC_ROOT}/bin/aarch64-none-linux-gnu-g++${EXE_SUFFIX}")
 
 set(CMAKE_ASM_COMPILER
-    "${ARM_GCC_ROOT}/bin/aarch64-none-linux-gnu-gcc")
-    
+    "${ARM_GCC_ROOT}/bin/aarch64-none-linux-gnu-gcc${EXE_SUFFIX}")
