a2827eed48e0a7aa7884ecb682ba0fe7df8d67e5
Elias Alexander Wiklund Ottersbo <realeawo5@gmail.com>
2026-09-23 17:29:23 +0200

    fix: add temp default construction of perception as it is currently missing implementation

--- src/drone.cpp
+++ src/drone.cpp
@@ -6,15 +6,16 @@
 // * Ctor & dtor
 Drone::Drone(M&& m) : m(std::move(m)) {} 
 Drone::~Drone() {
+    // Shutdown ROS context if it was initialized
     if (rclcpp::ok()) {
-        rclcpp::shutdown();                                                             // Shutdown ROS context if it was initialized
+        rclcpp::shutdown();                                                             
     }
 }
 
 
 std::expected<std::unique_ptr<Drone>, Drone::InitError>
 Drone::initiate(int argc, const char* argv[]) {
-    // 1. ROS global state - first thing, so subsystems can create nodes
+    // 1. Initialize ROS global state, so subsystems can create nodes
     if (!rclcpp::ok()) {
         rclcpp::init(argc, argv);
     }
@@ -22,6 +23,7 @@ Drone::initiate(int argc, const char* argv[]) {
     // 2. Subsystems (each may create rclcpp::Node objects)
     // auto perception = Perception::create();
     // if (!perception) return std::unexpected(InitError::CameraNotFound);
+    Perception perception;                                                              // | Debug as Perception is missing implementation
 
     // 3. Assemble
     // ? We wrap the raw pointer and let a unique_ptr take ownership to prevent leaks 
@@ -29,7 +31,7 @@ Drone::initiate(int argc, const char* argv[]) {
     // ? here because make_unique<Drone> is unavailable as the constructor is private
     return std::unique_ptr<Drone>(
         new Drone(M{                                                                    // ? We must return a Drone pointer, as the Drone 
-            .perception = std::move(*perception),                                       // ? object shouldn't be copyable nor movable, so 
+            .perception = std::move(perception),                                        // ? object shouldn't be copyable nor movable, so 
                                                                                         // ? we can't return a Drone object by value                            
         })                                                                                  
     );
