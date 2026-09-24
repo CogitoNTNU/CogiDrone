660c34670797d00261413494341ff56265c226af
Elias Alexander Wiklund Ottersbo <realeawo5@gmail.com>
2026-09-24 18:56:52 +0200

    chore: formatting

--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,16 @@ int main(int argc, const char* argv[]) {
         return 1;
     }
 
-    // `auto drone` is a std::expected<std::unique_ptr<Drone>, InitError>:
-    // drone : std::expected<std::unique_ptr<Drone>, InitError>
-    //      └── contains either:
-    //           ├── unique_ptr<Drone>  ->  Drone      (success)
-    //           └── InitError                         (failure) 
-    //
-    // To start the drone, we therefore must:
-    //  1. Dereference the expected    -> gives us the unique_ptr<Drone>
-    //  2. Dereference the unique_ptr  -> gives us the Drone&
-    //  3. Call the start() method on the Drone&
+    // ? `auto drone` is a std::expected<std::unique_ptr<Drone>, InitError>:
+    // ? drone : std::expected<std::unique_ptr<Drone>, InitError>
+    // ?      └── contains either:
+    // ?           ├── unique_ptr<Drone>  ->  Drone      (success)
+    // ?           └── InitError                         (failure) 
+    // ? 
+    // ? To start the drone, we therefore must:
+    // ?  1. Dereference the expected    -> gives us the unique_ptr<Drone>
+    // ?  2. Dereference the unique_ptr  -> gives us the Drone&
+    // ?  3. Call the start() method on the Drone&
 
     // Move the unique_ptr out of the expected once verified
     std::unique_ptr<Drone> drone = std::move(*result);                                  // Move the unique_ptr out of the expected once verified
