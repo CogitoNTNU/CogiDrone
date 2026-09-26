79045e19974976bd93c4ab9f69f04d9dd14433a7
Elias Alexander Wiklund Ottersbo <realeawo5@gmail.com>
2026-09-26 16:51:52 +0200

    feat: boilerplate perception class implemention

--- src/perception/perception.h
+++ src/perception/perception.h
@@ -1,12 +1,58 @@
 #pragma once
 
+#include <expected>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include "perception/sensors/camera.h"
+#include "perception/models/yolo.h"
+
+
+namespace perception {
+    struct Detection {
+        float x, y, w, h;
+        float confidence;
+        int class_id;
+    };
+};
 
 class Perception {
 private:   
-    
+    struct M {                                                                          // Assembly struct
+        // ROS node
+        rclcpp::Node::SharedPtr node;
+
+        // Subscriptions and publishers
+        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr imageSubscription;     // Subscription for camera images
+        rclcpp::Publisher<cogidrone::msg::FusedDetections>::SharedPtr targetPublisher;  // Publisher for fused target detections
+                
+        // Models
+        std::unique_ptr<Yolo> yolo;
+    } m;
+
+    explicit Perception(M&& m);    
 
 public:
-    Perception();
+    enum class InitError {
+        CameraNotFound,
+        ModelLoadFailed,
+    };
+    
+    
+    // * Ctors & dtor
+    [[nodiscard]] static std::expected<std::unique_ptr<Perception>, InitError> create();
+
     ~Perception();
+    Perception(const Perception&) = delete;
+    Perception& operator=(const Perception&) = delete;
+    Perception(Perception&&) = delete;
+    Perception& operator=(Perception&&) = delete;
+
+    // * Accessors
+    [[nodiscard]] inline rclcpp::Node::SharedPtr node() const noexcept { return m.node; }
+    
     
 };
